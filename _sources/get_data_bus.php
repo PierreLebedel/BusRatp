5e98417800d7e4989e5ffec584e3316a4d360799
Pierre Lebedel <pierrelebedel@gmail.com>
2016-03-22 23:10:49 +0100

    ligne 39

--- _sources/get_data_bus.php
+++ _sources/get_data_bus.php
@@ -1,5 +1,8 @@
 <?php
 $cmd = "/usr/bin/w3m -dump 'http://wap.ratp.fr/siv/schedule?service=next&reseau=bus&lineid=B69&stationid=69_657_708' | grep -A 1 \">\" | sed '/--/d' | sed 's/> //' | sed 's/  //g'";
+
+//piit: http://wap.ratp.fr/siv/schedule?reseau=bus&lineid=B39&stationid=39_16
+
 exec($cmd,$res);
 $data_mn= $res[5];
 if (count($res) == 0) {//bug in parsing w3m command or wap http maybe non available
